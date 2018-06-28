ae0d1e69c8e91f8a822a0c958110e6e03114acab
Unknown <sarneijim@gmail.com>
2018-06-28 14:26:07 -0400

    05 Use css Class with name
    Different examples

---
+++ _05.3cssClass.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: '#app',
+  data: {
+    attachRed: false,
+    color: 'green'
+  },
+  computed:{
+  	divClasses: function(){
+      return{
+        red: this.attachRed,
+        blue: !this.attachRed
+      };
+  	}
+  }
+});
+
---
+++ _05.4cssClass.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: '#app',
+  data: {
+    color: 'gray',
+    width: 100
+  },
+  computed:{
+    myStyle: function(){
+      return {
+        backgroundColor: this.color,
+        width: this.width + 'px'
+      }
+    }
+  }
+});
+
